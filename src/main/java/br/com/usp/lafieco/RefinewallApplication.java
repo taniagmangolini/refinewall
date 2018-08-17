e5f26fe00928ec1245a47bceb792ba64e2a75020
Tania Mangolini <taniagmangolini@gmail.com>
2018-08-17 13:57:28 -0300

    first

---
+++ src/main/java/br/com/usp/lafieco/RefinewallApplication.java
@@ -0,0 +1,12 @@
+package br.com.usp.lafieco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefinewallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefinewallApplication.class, args);
+	}
+}
