183b08931644967c30b8d84c1b8340d282843472
Tania Mangolini <taniagmangolini@gmail.com>
2018-08-23 14:04:02 -0300

    blast service

--- src/main/java/br/com/usp/lafieco/controller/BlastController.java
+++ src/main/java/br/com/usp/lafieco/controller/BlastController.java
@@ -5,7 +5,7 @@ import java.util.Locale;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,7 +28,7 @@ public class BlastController {
 	
 
 	@CrossOrigin
-	@GetMapping
+	@PostMapping
 	@ResponseBody
 	public String runBlast(
 			@RequestParam("sequence") String sequence, @RequestParam("email") String email ) {
