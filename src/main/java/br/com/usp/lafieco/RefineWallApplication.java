88105004d8267541d63074685d12778ebcaf861a
tgmangolini <taniagmangolini@gmail.com>
2018-10-07 20:52:57 -0300

    adjusts for deploy on wildfly

--- src/main/java/br/com/usp/lafieco/RefineWallApplication.java
+++ src/main/java/br/com/usp/lafieco/RefineWallApplication.java
@@ -2,6 +2,8 @@ package br.com.usp.lafieco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
@@ -13,7 +15,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {
         "br.com.usp.lafieco.repository"
         })
-public class RefineWallApplication {
+public class RefineWallApplication extends SpringBootServletInitializer {  //TO RUN ON WILDFLY
+	//public class PesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RefineWallApplication.class, args);
@@ -28,4 +31,9 @@ public class RefineWallApplication {
         return messageSource;
     }
 
+	//TO RUN ON WILDFLY 
+		@Override
+		protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+			return application.sources(RefineWallApplication.class);
+	}
 }
